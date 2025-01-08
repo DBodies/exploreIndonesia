b457cf355e050858b640366eaa40065416e51638
Denis <159953887+DBodies@users.noreply.github.com>
2025-01-08 21:03:10 +0200

    sixth section desktop

--- body.js
+++ body.js
@@ -1,35 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const images = [    // Масив изодражений
-        { src: './img/beachMountain@1x.jpg', srcset: './img/beachMountain@2x.jpg' },
-        { src: './img/sunVIDEO@1x.jpg', srcset: './img/sunVIDEO@2x.jpg' },
-        { src: './img/cityVIDEO@1x.jpg', srcset: './img/cityVIDEO@2x.jpg' },
-        { src: './img/photoGallery/Girls@1x.jpg', srcset: './img/photoGallery/Girls@2x.jpg' },
-        { src: './img/photoGallery/mountains@1x.jpg', srcset: './img/photoGallery/mountains@2x.jpg' },
+    const images = [    // Массив изображений
+        { src: './img/beachMountain@1x.jpg', srcset: './img/beachMountain@2x.jpg', desktopSrc: './img/field@1x.jpg', desktopSrcset: './img/field@2x.jpg' },
+        { src: './img/sunVIDEO@1x.jpg', srcset: './img/sunVIDEO@2x.jpg', desktopSrc: './img/beach@1x.jpg', desktopSrcset: './img/beach@2x.jpg' },
+        { src: './img/cityVIDEO@1x.jpg', srcset: './img/cityVIDEO@2x.jpg', desktopSrc: './img/rock@1x.jpg', desktopSrcset: './img/rock@2x.jpg' },
+        { src: './img/photoGallery/Girls@1x.jpg', srcset: './img/photoGallery/Girls@2x.jpg', desktopSrc: './img/children@1x.jpg', desktopSrcset: './img/children@2x.jpg' },
+        { src: './img/photoGallery/mountains@1x.jpg', srcset: './img/photoGallery/mountains@2x.jpg', desktopSrc: './img/picture@1x.jpg', desktopSrcset: './img/picture@2x.jpg' },
     ];
-    const imgElement = document.querySelector('.third-img');  // Тут обращаемся к фото которое мы меняе на фото с массива
-    const circles = document.querySelectorAll('.svg-circle'); // Это мы используем коллекцию свг для посдветки активного фото
-    let currentIndex = 0; // Переменная для отслеживания прогресса. ПО УМ = 0(то есть начинается с 1го фото)
+
+    const imgElement = document.querySelector('.third-img');  // Элемент изображения
+    const circles = document.querySelectorAll('.svg-circle'); // Коллекция svg-кружков для подсветки активного фото
+    let currentIndex = 0; // Индекс текущего изображения
+
+    // Функция обновления изображения и кружка
     function updateImageAndCircle(index) {
-        imgElement.src = images[index].src; // Меняем атриут src на новое значение из массива images по текущему индексу.
-        imgElement.srcset = images[index].srcset; // Для высококачественного фото  меняем атрибут src на новое значение из массива images по текущему индексу.
+        const isDesktop = window.innerWidth >= 1280; // Проверяем ширину экрана для десктопа
+        imgElement.src = isDesktop ? images[index].desktopSrc : images[index].src;
+        imgElement.srcset = isDesktop ? images[index].desktopSrcset : images[index].srcset;
+
         circles.forEach(circle => circle.classList.remove('selected'));
         circles[index].classList.add('selected');
     }
-    document.querySelector('.wrapper-back-next svg:last-child').addEventListener('click', function () {              //Ищем правую стрелку это последний ребёнок, добавляем обработчик событий на коик пр стрелку вправо
-        if (currentIndex < images.length - 1) {  // Смотри если это не последний изображение то увеличиваем индекс и вызываем функцию updateImageAndCircle(currentIndex);
+
+    // Обработчики для стрелок переключения
+    document.querySelector('.wrapper-back-next svg:last-child').addEventListener('click', function () {
+        if (currentIndex < images.length - 1) {
             currentIndex++;
             updateImageAndCircle(currentIndex);
         }
     });
-    document.querySelector('.wrapper-back-next svg:first-child').addEventListener('click', function () {  // Аналог стрелки вправо, только влево  
-        if (currentIndex > 0) { // Проверяем не является ли изображение первым, если нет то уменьшаем индекст и вызываем функцию  updateImageAndCircle(currentIndex);
+
+    document.querySelector('.wrapper-back-next svg:first-child').addEventListener('click', function () {
+        if (currentIndex > 0) {
             currentIndex--;
             updateImageAndCircle(currentIndex);
         }
     });
-    updateImageAndCircle(currentIndex);  // Что бы при загрузке страницы отобразить первое фото и подсведить иконку
-});
 
+    // Слушатель для обновления изображений при изменении размера окна
+    window.addEventListener('resize', () => updateImageAndCircle(currentIndex));
+
+    // Первоначальное обновление изображения и кружка при загрузке страницы
+    updateImageAndCircle(currentIndex);
+});
 
 
 
